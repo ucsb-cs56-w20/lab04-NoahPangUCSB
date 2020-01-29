451cca45709f8529040cf8f1ff71b0cf02388300
Noah Pang <noahpang@cs.ucsb.edu>
2020-01-28 17:42:06 -0800

    np - added controller method for multiplication results

--- src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
@@ -98,8 +98,6 @@ public class OperationsController {
             ratCalcForm.setNumResult(result.getNumerator());
             ratCalcForm.setDenomResult(result.getDenominator());
         }
-
-        // TODO: Fill this in with appropriate code
         
         model.addAttribute("ratCalcForm", ratCalcForm);
         return "operations/subtract";
@@ -109,6 +107,24 @@ public class OperationsController {
     public String getMultiply(Model model) {
         RatCalcForm ratCalcForm = new RatCalcForm();
         ratCalcForm.setOp("x");
+        model.addAttribute("ratCalcForm", ratCalcForm);
+        return "operations/multiply";
+    }
+
+    @GetMapping("/multiply/results")
+    public String getMultiplyResult(Model model, @Valid RatCalcForm ratCalcForm, BindingResult bindingResult){
+        logger.info("getMultiplyResult ratCalczform=" + ratCalcForm);
+        ratCalcForm.setOp("*");
+
+        if(!bindingResult.hasErrors() && !checkDenominatorErrors(ratCalcForm)){
+            Rational r1 = new Rational(ratCalcForm.getNum1(), ratCalcForm.getDenom1());
+            Rational r2 = new Rational(ratCalcForm.getNum2(), ratCalcForm.getDenom2());
+            Rational result = Rational.product(r1, r2);
+            logger.info("r1=" + r1 + " r2=" + r2 + " result=" + result);
+            ratCalcForm.setNumResult(result.getNumerator());
+            ratCalcForm.setDenomResult(result.getDenominator());
+        }
+
         model.addAttribute("ratCalcForm", ratCalcForm);
         return "operations/multiply";
     }
